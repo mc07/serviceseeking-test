2fc39276bc7baa7f456a54b854278a4b4ad2585a
mc07 <leu.myl7@gmail.com>
2016-10-07 09:47:53 +0800

    Updated create.rb
    Refactor Todo::Create

--- app/concepts/todo/create.rb
+++ app/concepts/todo/create.rb
@@ -12,39 +12,37 @@ class Todo::Create < Trailblazer::Operation
   #         associate `todo` with `todo_list`
   def process(params)
     # make sure title is not blank
-    return invalid! if params[:todo][:title].blank?
-
-    # build the todo
-    todo = @model = Todo.new(params.require(:todo).permit(:title, :description))
-
-    # find or create the parent list for the todo
-    if params[:todo_list_id].blank?
-      todo_list = TodoList.find_or_create_by(name: "Default To-do List")
-
-      # Find or create the owner of the todo_list:
-      ##### If current_user_id is present, fetch that user
-      ##### otherwise, create a new one
-      current_user =
-        if params[:current_user] == nil
-          if params[:current_user_id] == nil
-            User.create!(fullname: "Guest")
-          else
-            User.find(params[:current_user_id])
-          end
-        else
-          params[:current_user]
+    if params[:todo][:title].blank?
+      return invalid!
+    else
+      # build the todo
+      todo = Todo.new(params.require(:todo).permit(:title, :descriptioin))
+      # find or create the parent list for the todo
+      if params[:todo_list_id].present?
+        todo_list = TodoList.find(params[:todo_list_id])
+      else
+        todo_list = TodoList.find_by(name: "Default To-Do List")
+        if todo_list.blank?
+          todo_list = TodoList.create!(name: "Default To-Do List")
         end
-
-      # assign the todo_list to the user
-      todo_list.user = current_user
-      todo_list.save!
-    else # i.e. if params[:todo_list_id] is not nil
-      todo_list = TodoList.find(params[:todo_list_id])
+        # Find or create the owner of the todo_list:
+        ##### If current_user_id is present, fetch that user
+        ##### otherwise, create a new one
+        current_user = params[:current_user]
+        if params[:current_user].nil?
+          if params[:curent_user_id].nil?
+            current_user = User.create!(fullname: "Guest")
+          else
+            current_user = User.find(params[:current_user_id])
+          end
+        end
+        # assign the todo_list to the user
+        todo_list.user = current_user
+        todo_list.save!
+      end
+      # associate the todo with the todo_list
+      todo.list = todo_list
+      todo.save!
     end
-
-    # associate the todo with the todo_list
-    todo.list = todo_list
-    todo.save!
   end
-
 end
